3f539914fbb9f4d7b95fd111bb500f15d1168869
Alexandra <alexandra.abrego@control-it.mx>
2024-01-23 21:47:47 -0600

    Actividad API

--- JavaScript/API/fetch.js
+++ JavaScript/API/fetch.js
@@ -14,12 +14,28 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then(data=>{
     for(var i=0; i<5;i++){
        var  name="n_"+i
+       var  user="u_"+i
+       var email ="e_"+i
+       var phone ="p_"+i
+       var website="w_"+i
     
 
-    if(document.getElementById(name) != null){
+    if(document.getElementById(name) != null  && 
+       document.getElementById(user) != null  &&
+       document.getElementById(email) != null &&
+       document.getElementById(phone) != null &&
+       document.getElementById(website) != null ){
+           
             objetivo=document.getElementById(name).innerHTML=data[i].name
-            console.log(data[i].name)
+            objetivo=document.getElementById(user).innerHTML=data[i].username
+            objetivo=document.getElementById(email).innerHTML=data[i].email
+            objetivo=document.getElementById(phone).innerHTML=data[i].phone
+            objetivo=document.getElementById(website).innerHTML=data[i].website
+
+        
     }
     }
 })
+
+
 
